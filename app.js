6a0d5e2b6266648305eb015099d40e9139270e83
mikejdavo <mikejdavo@gmail.com>
2022-12-07 16:11:31 +0200

    commit 	modified:   app.js

--- app.js
+++ app.js
@@ -71,7 +71,8 @@ class App {
 
     this.ui.start('#firebaseui-auth-container', {
       signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       ],
     });
   }
